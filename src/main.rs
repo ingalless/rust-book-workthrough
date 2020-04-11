9738e5f7db357f85157ff2aec6fee698ce617474
Jonny <jonathanaingall@gmail.com>
2020-04-11 14:36:01 +0000

    References and borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,26 @@
 fn main() {
-    let s = String::from("hello"); // s comes into scope
+    let s = String::from("Hello");
 
-    let s = takes_ownership_and_gives_back(s); // s's value moves into the function...
-    println!("{}", s); // This can work now and is good because we minimise the work of the heap
+    let len = calculate_length(&s);
 
-    let x = 5; // x comes into scope
+    let mut mutatable_s = String::from("Hello");
+    let mutated_len = modify_and_calculate_length(&mut mutatable_s);
 
-    makes_copy(x); // x would move into the function,
-                   // but i32 is Copy, so it’s okay to still
-                   // use x afterward
-    println!("{}", x);
-} // Here, x goes out of scope, then s. But because s's value was moved, nothing
-  // special happens.
+    println!(
+        "The length of '{}' is {} and mutatable is '{}' with length of {}",
+        s, len, mutatable_s, mutated_len
+    );
+}
 
-fn takes_ownership_and_gives_back(some_string: &String) -> String {
-    // some_string comes into scope
-    println!("{}", some_string);
-    some_string
-} // We can give ownership back by returning!
+// In the below function s is "borrowed" from the calling scope...
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+// So drop isn't called here, meaning it's still in scope in main()
 
-fn makes_copy(some_integer: i32) {
-    // some_integer comes into scope
-    println!("{}", some_integer);
-} // Here, some_integer goes out of scope. Nothing special happens.
+// This example is the same as above, only we're mutating the variable passed in
+fn modify_and_calculate_length(s: &mut String) -> usize {
+    s.push_str(", world");
+    s.len()
+}
+// This will mutate the value in the calling scope
