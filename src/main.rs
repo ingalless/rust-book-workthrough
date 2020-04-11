a5127e051a6cd38fd29222e192b44c5af2a457a3
Jonny <jonathanaingall@gmail.com>
2020-04-11 12:32:56 +0000

    Add example of cloning

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,9 @@ fn main() {
 }
 
 fn pointers() {
-    // When we do the below, because the values get pushed to the stack, we can just push 2 individual values.
-    // Namely, x = 5, y = 5
-    let x = 5;
-    let y = x;
-
-    // But in this case, s1 would go on the heap. It would be expensive to push both on the heap.
-    // So instead, s2 will copy the pointer, length and capacity from s1, but NOT the data.
+    // If we really want to copy the data too, then you can use clone()!
     let s1 = String::from("Hello");
-    let s2 = s1;
+    let s2 = s1.clone();
 
-    // But something else important happens. The reference to s1 gets lost.
-    // Therefore, we can't do "println!("{}", sl);" because the ownership has moved to s2 now.
+    println!("one {} and two {}", s1, s2);
 }
