f5be0cff32dc322e046226dda8d824aad121187d
Jonny <jonathanaingall@gmail.com>
2020-04-11 09:23:52 +0000

    Finish chapter 3.5: Control flow

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,55 @@ fn main() {
     }
 
     println!("The value of this is {}", conditional());
+    a_loop();
+    lift_off();
+    for_loop();
+    a_range();
 }
 
 fn conditional() -> i32 {
     let condition = false;
     let number = if condition { 5 } else { 6 };
     number
+}
+
+fn a_loop() {
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            // Result gets assigned the value of the break return
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
+
+fn lift_off() {
+    let mut counter = 3;
+
+    while counter != 0 {
+        println!("{}!", counter);
+        counter -= 1;
+    }
+
+    println!("LIFT OFF!");
+}
+
+fn for_loop() {
+    let a = [10, 20, 30, 40, 50];
+
+    for element in a.iter() {
+        println!("The value is: {}", element);
+    }
+}
+
+fn a_range() {
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+
+    println!("Lifted off with cleaner code!");
 }
