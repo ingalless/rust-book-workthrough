ff42843648a18b460059337dbed77627be3f47ca
Jonny <jonathanaingall@gmail.com>
2020-04-11 12:54:19 +0000

    Demonstrate ownership being given back

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let s = String::from("hello"); // s comes into scope
 
-    takes_ownership(s); // s's value moves into the function...
-                        // println!("{}", s); // ... and so is no longer valid here
+    let s = takes_ownership_and_gives_back(s); // s's value moves into the function...
+    println!("{}", s); // This can work now and is good because we minimise the work of the heap
 
     let x = 5; // x comes into scope
 
@@ -13,11 +13,11 @@ fn main() {
 } // Here, x goes out of scope, then s. But because s's value was moved, nothing
   // special happens.
 
-fn takes_ownership(some_string: String) {
+fn references_string(some_string: &String) -> String {
     // some_string comes into scope
     println!("{}", some_string);
-} // Here, some_string goes out of scope and `drop` is called. The backing
-  // memory is freed.
+    some_string
+} // We can give ownership back by returning!
 
 fn makes_copy(some_integer: i32) {
     // some_integer comes into scope
