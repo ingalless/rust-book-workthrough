130bc88b5f2581fc2b99e997ef7da616a56827a8
Jonny <jonathanaingall@gmail.com>
2020-04-12 16:30:58 +0000

    Adding methods to structs

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,40 @@ struct Rectangle {
     width: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        self.area() > rect.area()
+    }
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
 fn main() {
     let rectangle = Rectangle {
         height: 30,
         width: 50,
     };
 
-    println!("The area of the rectangle is {}", area(&rectangle));
+    let small_rect = Rectangle {
+        height: 20,
+        width: 30,
+    };
+
+    let square = Rectangle::square(20);
+
+    println!("The area of the rectangle is {}", rectangle.area());
     println!("Rectangle is {:?}", rectangle);
     println!("And even prettier it is {:#?}", rectangle);
-}
-
-fn area(rect: &Rectangle) -> u32 {
-    rect.height * rect.width
+    println!(
+        "Rectangle 1 can hold Rectangle 2 is {}",
+        rectangle.can_hold(&small_rect)
+    );
+    println!("Our square {:?}", square);
 }
