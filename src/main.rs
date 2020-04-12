b93914d56803f5b3014f0c79de9a44fdd1e02e3b
Jonny <jonathanaingall@gmail.com>
2020-04-12 16:17:58 +0000

    Tuple structs

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,10 @@
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
 
 fn main() {
-    let mut user = User {
-        email: String::from("jonathan@ingallnet.co.uk"),
-        username: String::from("ingalless"),
-        active: true,
-        sign_in_count: 1,
-    };
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
 
-    // because the struct is mutable, the fields are mutable too
-    user.email = String::from("jennifer@ingallnet.co.uk");
-
-    let user2 = build_user(String::from("jonny@ingall.com"), String::from("ingalless"));
-    let user3 =
-        build_new_from_existing_user(String::from("email"), String::from("username"), user2);
-}
-
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
-
-// Below illustrates update syntax, kind of like javascript object spread!
-fn build_new_from_existing_user(email: String, username: String, user: User) -> User {
-    User {
-        email,
-        username,
-        ..user
-    }
+    // Although the above tuples are identical in signature,
+    // you wouldn't be able to use a Point instead of a Color in a function
 }
